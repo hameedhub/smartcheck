5e0e87da56438040b5a736f31d15496db21530b4
Hameed Abdulrahaman <hameedabdulrahamann@gmail.com>
2019-08-03 17:47:04 +0100

    MVC structure

--- views/error/index.php
+++ views/error/index.php
@@ -1,6 +1,13 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Error Page</title>
+</head>
 <body>
-    <h1> Page Not Found </h1>
+    <h1>Page Not Found</h1>
     <hr>
     <?php echo $this->msg ?>
 </body>
